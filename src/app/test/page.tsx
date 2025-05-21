6a7b8ec98f9a8892dbcaf695c5b218cce9fbc324
John Paul Guiuan <jpguiuan10@gmail.com>
2025-05-21 09:54:19 -0700

    design

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+export default function TestPage() {
+  return (
+    <div className="p-8">
+      <div className="bg-red-500 p-4 rounded-lg text-white mb-4">
+        This should be a red box with white text
+      </div>
+      <div className="bg-blue-500 p-4 rounded-lg text-white">
+        This should be a blue box with white text
+      </div>
+    </div>
+  );
+} 
